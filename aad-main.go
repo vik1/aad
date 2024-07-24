e8bcba5efc54363c10406df32d8c92b0b154ae04
vik1 <vikrantverma99@gmail.com>
2024-07-24 15:41:56 +0530

    Update aad-main.go

--- aad-main.go
+++ aad-main.go
@@ -4,19 +4,19 @@ import (
 	"context"
 	"os"
 	"time"
-
+        "github.com/Azure/azure-sdk-for-go/sdk/azidentity"
 	"github.com/Azure/azure-sdk-for-go/sdk/keyvault/azsecrets"
-	"k8s.io/klog/v2"
+	"log"
 )
 
 func main() {
 	keyvaultURL := os.Getenv("KEYVAULT_URL")
 	if keyvaultURL == "" {
-		klog.Fatal("KEYVAULT_URL environment variable is not set")
+		log.Fatal("KEYVAULT_URL environment variable is not set")
 	}
 	secretName := os.Getenv("SECRET_NAME")
 	if secretName == "" {
-		klog.Fatal("SECRET_NAME environment variable is not set")
+		log.Fatal("SECRET_NAME environment variable is not set")
 	}
 
 	// Azure AD Workload Identity webhook will inject the following env vars
@@ -31,36 +31,36 @@ func main() {
 	authorityHost := os.Getenv("AZURE_AUTHORITY_HOST")
 
 	if clientID == "" {
-		klog.Fatal("AZURE_CLIENT_ID environment variable is not set")
+		log.Fatal("AZURE_CLIENT_ID environment variable is not set")
 	}
 	if tenantID == "" {
-		klog.Fatal("AZURE_TENANT_ID environment variable is not set")
+		log.Fatal("AZURE_TENANT_ID environment variable is not set")
 	}
 	if tokenFilePath == "" {
-		klog.Fatal("AZURE_FEDERATED_TOKEN_FILE environment variable is not set")
+		log.Fatal("AZURE_FEDERATED_TOKEN_FILE environment variable is not set")
 	}
 	if authorityHost == "" {
-		klog.Fatal("AZURE_AUTHORITY_HOST environment variable is not set")
+		log.Fatal("AZURE_AUTHORITY_HOST environment variable is not set")
 	}
 
 	cred, err := newClientAssertionCredential(tenantID, clientID, authorityHost, tokenFilePath, nil)
 	if err != nil {
-		klog.Fatal(err)
+		log.Fatal(err)
 	}
 
 	// initialize keyvault client
 	client, err := azsecrets.NewClient(keyvaultURL, cred, &azsecrets.ClientOptions{})
 	if err != nil {
-		klog.Fatal(err)
+		log.Fatal(err)
 	}
 
 	for {
 		secretBundle, err := client.GetSecret(context.Background(), secretName, "", nil)
 		/* if err != nil {
-			klog.ErrorS(err, "failed to get secret from keyvault", "keyvault", keyvaultURL, "secretName", secretName)
+			log.ErrorS(err, "failed to get secret from keyvault", "keyvault", keyvaultURL, "secretName", secretName)
 			os.Exit(1)
 		} */
-		klog.InfoS("successfully got secret", "secret", *secretBundle.Value)
+		log.InfoS("successfully got secret", "secret", *secretBundle.Value)
 
 		// wait for 60 seconds before polling again
 		time.Sleep(60 * time.Second)
